c841e5b93ab4887480a037546244136a3bbde8d0
HP 830 G5 <ibrahimnashat2634@gmail.com>
2023-08-02 15:41:48 +0200

    update

--- lib/src/read_imports_file.dart
+++ lib/src/read_imports_file.dart
@@ -13,8 +13,7 @@ class ReadImports {
           .split('part')[0]
           .replaceFirst("import 'package:annotations/annotations.dart';", '')
           .replaceFirst("import 'package:dio/dio.dart';", '')
-          .replaceFirst("import 'package:retrofit/http.dart';", '')
-          .replaceAll("'../", "'../../");
+          .replaceFirst("import 'package:retrofit/http.dart';", '');
       return content;
     }
     throw Exception('File not exist');
