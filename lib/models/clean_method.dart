dbb816b498e8deaee044d05b6dd466ed60e41d1b
HP 830 G5 <ibrahimnashat2634@gmail.com>
2023-08-06 18:26:16 +0200

    update

--- lib/models/clean_method.dart
+++ lib/models/clean_method.dart
@@ -1,10 +1,10 @@
-enum MethodType { GET, POST, DELETE, PATCH, POST_MULTI_PART }
+enum MethodType { POST, GET, DELETE, PATCH, POST_MULTI_PART }
 
-enum RequestType { Body, OneFiled }
+enum RequestType { Fields, Body }
 
 enum ParamType { Filed, Query, Path }
 
-enum ParamProp { Set, EmitSet, TextController, none }
+enum ParamProp { none, Set, EmitSet, TextController }
 
 class CleanMethod<T> {
   final String name;
@@ -18,23 +18,67 @@ class CleanMethod<T> {
     required this.name,
     required this.endPoint,
     this.methodType = MethodType.POST,
-    this.requestType = RequestType.OneFiled,
+    this.requestType = RequestType.Fields,
     this.isCache = false,
     this.isPaging = false,
     this.parameters = const [],
   });
+
+  factory CleanMethod.fromJson(Map<String, dynamic> map) {
+    final methodTypeIndex = MethodType.values
+        .indexWhere((element) => element.name == map['methodType']);
+    final requestTypeIndex = RequestType.values
+        .indexWhere((element) => element.name == map['requestType']);
+
+    final methodType =
+        MethodType.values[methodTypeIndex == -1 ? 0 : methodTypeIndex];
+    final requestType =
+        RequestType.values[requestTypeIndex == -1 ? 0 : requestTypeIndex];
+
+    List<Param> params = [];
+    for (var item in map['parameters'] ?? []) {
+      params.add(Param.fromJson(item));
+    }
+
+    return CleanMethod(
+      name: map['name'],
+      endPoint: map['endPoint'],
+      methodType: methodType,
+      requestType: requestType,
+      parameters: params,
+      isPaging: map['isPaging'] ?? false,
+      isCache: map['isCache'] ?? false,
+    );
+  }
 }
 
 class Param {
   final String name;
-  final bool isRequired;
   final ParamType type;
   final ParamProp prop;
+  final bool isRequired;
 
   const Param({
     required this.name,
-    this.type = ParamType.Filed,
-    this.prop = ParamProp.none,
+    required this.type,
+    required this.prop,
     this.isRequired = true,
   });
+
+  factory Param.fromJson(Map<String, dynamic> json) {
+    final typeIndex =
+        ParamType.values.indexWhere((element) => element.name == json['type']);
+    final propIndex =
+        ParamProp.values.indexWhere((element) => element.name == json['prop']);
+
+    final paramType = ParamType.values[typeIndex == -1 ? 0 : typeIndex];
+    final paramProp = ParamProp.values[propIndex == -1 ? 0 : propIndex];
+
+    return Param(
+      name: json['name'],
+      type: paramType,
+      prop: paramProp,
+      isRequired: json['isRequired'] ?? true,
+    );
+  }
 }
--- lib/src/model_visitor.dart
+++ lib/src/model_visitor.dart
@@ -1,10 +1,15 @@
 // ignore_for_file: depend_on_referenced_packages
 
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/visitor.dart';
 import 'package:clean_architecture_generator/models/usecase_model.dart';
 
-class ModelVisitor extends SimpleElementVisitor<void> {
+import '../models/clean_method.dart';
+
+class ModelVisitor extends GeneralizingElementVisitor<void> {
   String className = '';
   List<UseCaseModel> useCases = [];
   String data = "";
@@ -17,14 +22,45 @@ class ModelVisitor extends SimpleElementVisitor<void> {
 
   @override
   void visitMethodElement(MethodElement element) {
-    data =
-        "${element.children.map((e) => e.declaration.toString()).toList().toString()}";
+    final path =
+        element.declaration.source.toString().replaceFirst("/example/", "");
+    final method = File(path).readAsStringSync();
+    final pattern = RegExp('\\s+');
+    final items = method
+        .substring(method.indexOf('['), method.indexOf('];') + 1)
+        .trim()
+        .replaceAll("\n", "")
+        .replaceAll(pattern, "")
+        .replaceAll('const', "")
+        .replaceAll(")", '"}')
+        .replaceAll('CleanMethod(', '{"')
+        .replaceAll(',', ',"')
+        .replaceAll('Param(', '{"')
+        .replaceAll('MethodType.', '"')
+        .replaceAll('RequestType.', '"')
+        .replaceAll('ParamType.', '"')
+        .replaceAll('ParamProp.', '"')
+        .replaceAll(':', '":')
+        .replaceAll("'", '"')
+        .replaceAll(',', '",')
+        .replaceAll('""', '"')
+        .replaceAll(',"}', "}")
+        .replaceAll('","]"', "]")
+        .replaceAll('","]', "]")
+        .replaceAll('}","{', "},{");
+
+    List<CleanMethod> methods = [];
+    for (var item in jsonDecode(items)) {
+      methods.add(CleanMethod.fromJson(item));
+    }
+
+    data += methods.map((e) => e.name).toList().toString();
+
     final parameters = [...element.parameters];
     final type = element.returnType.toString();
     final name = element.name.toString();
     final comment = element.documentationComment ?? '';
     final prop = propIn(comment: comment, name: 'Prop');
-
     final textControllers =
         varsIn(comment: comment, name: 'TextController', params: parameters);
 
