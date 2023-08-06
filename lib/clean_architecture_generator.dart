948688a61a82e179f90ce10140a27824728de0e9
HP 830 G5 <ibrahimnashat2634@gmail.com>
2023-08-07 00:38:04 +0200

    update

--- lib/clean_architecture_generator.dart
+++ lib/clean_architecture_generator.dart
@@ -3,7 +3,21 @@ library mvvm_generator;
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 
+import 'src/generators/cache_cubit_generator.dart';
+import 'src/generators/cache_usecase_generator.dart';
+import 'src/generators/cubit_generator.dart';
+import 'src/generators/local_data_source_generator.dart';
 import 'src/generators/optimize/optimize_generator.dart';
+import 'src/generators/repository_generator.dart';
+import 'src/generators/requests_generator.dart';
+import 'src/generators/test/cache_cubit_test_generator.dart';
+import 'src/generators/test/cache_usecase_test_generator.dart';
+import 'src/generators/test/cubit_test_generator.dart';
+import 'src/generators/test/get_cache_usecase_test_generator.dart';
+import 'src/generators/test/local_data_source_test_generator.dart';
+import 'src/generators/test/repository_test_generator.dart';
+import 'src/generators/test/usecase_test_generator.dart';
+import 'src/generators/usecase_generator.dart';
 
 export 'models/clean_method.dart';
 export 'src/annotations.dart';
@@ -12,28 +26,28 @@ export 'src/generators/main/clean_architecture_set_up.dart';
 Builder generateCleanArchitecture(BuilderOptions options) => SharedPartBuilder(
       [
         OptimizeGenerator(),
-        // RequestsGenerator(),
-        // LocalDataSourceGenerator(),
-        // RepositoryGenerator(),
-        // CacheUseCaseGenerator(),
-        // UseCaseGenerator(),
-        // CubitGenerator(),
-        // CacheCubitGenerator(),
-        //
-        // ///[Test]
-        // RepositoryTestGenerator(),
-        // LocalDataSourceTestGenerator(),
-        // UseCaseTestGenerator(),
-        // CacheUseCaseTestGenerator(),
-        // GetCacheUseCaseTestGenerator(),
-        // CubitTestGenerator(),
-        // CacheCubitTestGenerator(),
+        RequestsGenerator(),
+        LocalDataSourceGenerator(),
+        RepositoryGenerator(),
+        CacheUseCaseGenerator(),
+        UseCaseGenerator(),
+        CubitGenerator(),
+        CacheCubitGenerator(),
+
+        ///[Test]
+        RepositoryTestGenerator(),
+        LocalDataSourceTestGenerator(),
+        UseCaseTestGenerator(),
+        CacheUseCaseTestGenerator(),
+        GetCacheUseCaseTestGenerator(),
+        CubitTestGenerator(),
+        CacheCubitTestGenerator(),
 
         // ///[Move Remote data source]
         // MoveRemoteDataSourceGenerator()
       ],
       'clean_architecture',
-      // formatOutput: (code) {
-      //   return '#BUILD DONE SUCCESSFULLY';
-      // },
+      formatOutput: (code) {
+        return '#BUILD DONE SUCCESSFULLY';
+      },
     );
