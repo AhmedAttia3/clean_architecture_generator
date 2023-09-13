96a12924f35fa6c98e25ffbe44cfaa5a149233cc
HP 830 G5 <ibrahimnashat2634@gmail.com>
2023-09-14 01:31:48 +0200

    update

--- lib/formatter/method_format.dart
+++ lib/formatter/method_format.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:clean_architecture_generator/src/models/usecase_model.dart';
 
 class MethodFormat {
@@ -82,7 +84,9 @@ class MethodFormat {
     } else if (type == 'num') {
       return 0.0;
     } else if (type.contains("List")) {
-      return [];
+      return const [];
+    } else if (type.contains("File")) {
+      return File('');
     }
   }
 }
