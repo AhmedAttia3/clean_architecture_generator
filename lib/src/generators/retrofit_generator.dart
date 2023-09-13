e6241a5597ec05933cce2b65270307080d69cff5
HP 830 G5 <ibrahimnashat2634@gmail.com>
2023-09-14 01:27:49 +0200

    update

--- lib/src/generators/retrofit_generator.dart
+++ lib/src/generators/retrofit_generator.dart
@@ -73,7 +73,7 @@ class RetrofitGenerator extends GeneratorForAnnotation<ArchitectureAnnotation> {
               method.methodType == MethodType.PUT_MULTI_PART) {
             for (var param in method.requestParameters) {
               clientServices.writeln(
-                  "         @Part(name: ${param.dataType == ParamDataType.List || param.dataType == ParamDataType.listFile ? '${param.key}[]' : '${param.key}'}) ${param.isRequired ? "required ${param.dataType == ParamDataType.listFile ? 'List<File>' : param.dataType.name}" : "${param.dataType == ParamDataType.listFile ? 'List<File>' : param.dataType.name}?"}  ${param.name},");
+                  "         @Part(name: '${param.dataType == ParamDataType.List || param.dataType == ParamDataType.listFile ? '${param.key}[]' : '${param.key}'}') ${param.isRequired ? "required ${param.dataType == ParamDataType.listFile ? 'List<File>' : param.dataType.name}" : "${param.dataType == ParamDataType.listFile ? 'List<File>' : param.dataType.name}?"}  ${param.name},");
             }
           } else {
             for (var param in method.requestParameters) {
