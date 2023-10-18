a229253f9e6a7c999b898fe50a3e9ba3ca54a0de
HP 830 G5 <ibrahimnashat2634@gmail.com>
2023-10-18 17:53:36 +0300

    update

--- example/lib/home/data/repository/home_repository_impl.dart
+++ example/lib/home/data/repository/home_repository_impl.dart
@@ -1,49 +1,30 @@
-import 'dart:io';
 import 'package:eitherx/eitherx.dart';
-import 'package:mwidgets/mwidgets.dart';
-import 'package:example/core/base/base_response.dart';
-import 'package:example/core/base/no_params.dart';
-import 'package:injectable/injectable.dart';
-import 'package:example/core/base/base_use_case.dart';
-import 'dart:convert';import 'package:example/home/domain/repository/home_repository.dart';
-import 'package:example/home/domain/repository/home_repository.dart';
-import 'package:example/home/data/data-sources/home_remote_data_source.dart';
-import 'package:example/home/domain/entities/governorate_entity.dart';
-import 'package:example/home/data/models/governorate_model.dart';
-import 'package:example/home/domain/entities/result_entity.dart';
-import 'package:example/home/data/models/result_model.dart';
-import 'package:example/home/domain/entities/device_settings_entity.dart';
+import 'package:example/home/data/data-sources/home_local_data_source.dart';
 import 'package:example/home/data/models/device_settings_model.dart';
-import 'package:example/core/base/base_response.dart';
+import 'package:example/home/domain/entities/device_settings_entity.dart';
+import 'package:example/home/domain/repository/home_repository.dart';
+import 'package:injectable/injectable.dart';
+import 'package:mwidgets/mwidgets.dart';
 
 ///[HomeRepositoryImplement]
 ///[Implementation]
-@Injectable(as:HomeRepository)
+@Injectable(as: HomeRepository)
 class HomeRepositoryImplement implements HomeRepository {
-final HomeRemoteDataSource homeRemoteDataSource;
-const HomeRepositoryImplement(
-this.homeRemoteDataSource,
-);
+  final HomeLocalDataSource homeLocalDataSource;
 
-@override
-Future<Either<Failure, BaseResponse<List<GovernorateEntity>?>>> getGovernorates()async {
-return await homeRemoteDataSource.getGovernorates();
+  const HomeRepositoryImplement(
+    this.homeLocalDataSource,
+  );
+
+  @override
+  Future<Either<Failure, Unit>> cacheUpdateUser({
+    required DeviceSettingsModel data,
+  }) async {
+    return await homeLocalDataSource.cacheUpdateUser(data: data);
+  }
+
+  @override
+  Either<Failure, DeviceSettingsEntity> getCacheUpdateUser() {
+    return homeLocalDataSource.getCacheUpdateUser();
+  }
 }
-
-@override
-Future<Either<Failure, BaseResponse<ResultEntity?>>> getResult({required int  countryId,int?  termId,String?  studentName,String?  sittingNumber, })async {
-return await homeRemoteDataSource.getResult(countryId: countryId,termId: termId,studentName: studentName,sittingNumber: sittingNumber,);
-}
-
-@override
-Future<Either<Failure, BaseResponse<int>>> addFavorite({required int  countryId, })async {
-return await homeRemoteDataSource.addFavorite(countryId: countryId,);
-}
-
-@override
-Future<Either<Failure, BaseResponse<DeviceSettingsEntity?>>> updateUser({required int  firebaseToken, })async {
-return await homeRemoteDataSource.updateUser(firebaseToken: firebaseToken,);
-}
-
-}
-
--- example/lib/home/domain/repository/home_repository.dart
+++ example/lib/home/domain/repository/home_repository.dart
@@ -1,22 +1,13 @@
-import 'dart:io';
 import 'package:eitherx/eitherx.dart';
-import 'package:mwidgets/mwidgets.dart';
-import 'package:example/core/base/base_response.dart';
-import 'package:example/core/base/no_params.dart';
-import 'package:injectable/injectable.dart';
-import 'package:example/core/base/base_use_case.dart';
-import 'package:example/home/domain/entities/governorate_entity.dart';
-import 'package:example/home/data/models/governorate_model.dart';
-import 'package:example/home/domain/entities/result_entity.dart';
-import 'package:example/home/data/models/result_model.dart';
-import 'package:example/home/domain/entities/device_settings_entity.dart';
 import 'package:example/home/data/models/device_settings_model.dart';
+import 'package:example/home/domain/entities/device_settings_entity.dart';
+import 'package:mwidgets/mwidgets.dart';
 
 ///[Implementation]
 abstract class HomeRepository {
-Future<Either<Failure, BaseResponse<List<GovernorateEntity>?>>> getGovernorates();
-Future<Either<Failure, BaseResponse<ResultEntity?>>> getResult({required int  countryId,int?  termId,String?  studentName,String?  sittingNumber, });
-Future<Either<Failure, BaseResponse<int>>> addFavorite({required int  countryId, });
-Future<Either<Failure, BaseResponse<DeviceSettingsEntity?>>> updateUser({required int  firebaseToken, });
-}
+  Future<Either<Failure, Unit>> cacheUpdateUser({
+    required DeviceSettingsModel data,
+  });
 
+  Either<Failure, DeviceSettingsEntity> getCacheUpdateUser();
+}
