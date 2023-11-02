821031adcb1f6cc80df67ff38b532276dae2522c
HP 830 G5 <ibrahimnashat2634@gmail.com>
2023-11-02 11:21:41 +0200

    update

--- lib/src/generators/cubit_states.dart
+++ lib/src/generators/cubit_states.dart
@@ -5,7 +5,7 @@ const loadingState = 'const LoadingState(type: LoadingRendererType.popup)';
 const contentState = 'const ContentState()';
 
 const successState =
-    'SuccessState(type: SuccessRendererType.content,message: data.message,)';
+    'SuccessState(type: SuccessRendererType.none,message: data.message,)';
 
 const errorState =
     'ErrorState(type: ErrorRendererType.toast,message: data.message,)';
@@ -14,7 +14,7 @@ const errorFailureState =
     'ErrorState(type: ErrorRendererType.toast,message: failure.message,)';
 
 const successStateTest =
-    'SuccessState(type: SuccessRendererType.content,message: "message",)';
+    'SuccessState(type: SuccessRendererType.none,message: "message",)';
 
 const errorStateTest =
     'ErrorState(type: ErrorRendererType.toast,message: "message",)';
