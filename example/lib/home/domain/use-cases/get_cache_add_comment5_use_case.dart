049832e6c6361caf3f23726fcfe2c9c0fd666726
HP 830 G5 <ibrahimnashat2634@gmail.com>
2023-11-02 15:56:29 +0200

    update

--- example/lib/home/domain/use-cases/get_cache_add_comment5_use_case.dart
+++ example/lib/home/domain/use-cases/get_cache_add_comment5_use_case.dart
@@ -1,27 +1,118 @@
-import 'dart:ffi';
-
+import 'dart:io';
 import 'package:eitherx/eitherx.dart';
-import 'package:example/core/base/base_use_case.dart';
-import 'package:example/home/domain/entities/device_settings_entity.dart';
-import 'package:example/home/domain/repository/home_repository.dart';
-import 'package:injectable/injectable.dart';
 import 'package:mwidgets/mwidgets.dart';
+import 'package:example/core/base/base_response.dart';
+import 'package:example/core/base/no_params.dart';
+import 'package:injectable/injectable.dart';
+import 'package:example/core/base/base_use_case.dart';
+import 'dart:ffi';
+import 'package:example/home/domain/repository/home_repository.dart';
+import 'package:example/home/domain/use-cases/get_cache_add_comment5_use_case.dart';
+import 'package:example/home/domain/entities/device_settings_entity.dart';
+import 'package:example/home/data/models/device_settings_model.dart';
+import 'package:example/home/domain/entities/device_settings_entity.dart';
+import 'package:example/home/data/models/device_settings_model.dart';
+import 'package:example/home/domain/entities/device_settings_entity.dart';
+import 'package:example/home/data/models/device_settings_model.dart';
+import 'package:example/home/domain/entities/device_settings_entity.dart';
+import 'package:example/home/data/models/device_settings_model.dart';
+import 'package:example/home/domain/entities/device_settings_entity.dart';
+import 'package:example/home/data/models/device_settings_model.dart';
+import 'package:example/home/domain/entities/device_settings_entity.dart';
+import 'package:example/home/data/models/device_settings_model.dart';
+import 'package:example/home/domain/entities/device_settings_entity.dart';
+import 'package:example/home/data/models/device_settings_model.dart';
+import 'package:example/home/domain/entities/device_settings_entity.dart';
+import 'package:example/home/data/models/device_settings_model.dart';
 
 ///[GetCacheAddComment5UseCase]
 ///[Implementation]
 @injectable
-class GetCacheAddComment5UseCase
-    implements BaseUseCase<Void?, Either<Failure, DeviceSettingsEntity>> {
-  final HomeRepository repository;
+class GetCacheAddComment5UseCase implements BaseUseCase<Void?, Either<Failure, DeviceSettingsEntity>>{
+final HomeRepository repository;
+const GetCacheAddComment5UseCase(
+this.repository,
+);
 
-  const GetCacheAddComment5UseCase(
-    this.repository,
-  );
 
-  @override
-  Either<Failure, DeviceSettingsEntity> execute({
-    Void? request,
-  }) {
-    return repository.getCacheAddComment5();
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@override
+Either<Failure, DeviceSettingsEntity> execute({Void? request,}) {
+return repository.getCacheAddComment5();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
